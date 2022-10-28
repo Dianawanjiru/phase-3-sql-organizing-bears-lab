904b184f80b01d82b0b72106a1057635f97b8594
Diana <diana.wanjiru@student.moringaschool.com>
2022-10-28 08:06:23 +0300

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, sex) VALUES ("Mr.Chocolate", "M");
+INSERT INTO bears (name, sex) VALUES ("Rowdy", "M");
+INSERT INTO bears (name, sex) VALUES ("Tabitha", "F");
+INSERT INTO bears (name, sex) VALUES ("Sergent Brown", "M");
+INSERT INTO bears (name, sex) VALUES ("Mellissa", "F");
+INSERT INTO bears (name, sex) VALUES ("Grinch", "M");
+INSERT INTO bears (name, sex) VALUES ("Wendy", "F");
+INSERT INTO bears (name, sex) VALUES (NULL, "M");
+
+
